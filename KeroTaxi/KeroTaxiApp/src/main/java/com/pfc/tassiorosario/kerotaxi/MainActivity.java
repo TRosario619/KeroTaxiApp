e9691fded7ea512bebd52d054d6b69f6c24a939c
TRosario619 <tassiorosario@gmail.com>
2016-08-09 22:21:10 +0200

    Update 10/07/2016 Registry progress 90%

--- KeroTaxi/KeroTaxiApp/src/main/java/com/pfc/tassiorosario/kerotaxi/MainActivity.java
+++ KeroTaxi/KeroTaxiApp/src/main/java/com/pfc/tassiorosario/kerotaxi/MainActivity.java
@@ -1,10 +1,19 @@
 package com.pfc.tassiorosario.kerotaxi;
 
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,21 +22,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.backendless.Backendless;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback
-{
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
-
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+            Location lastLoc;
     private GoogleMap gMap;
+    private SupportMapFragment mapFragment;
+    private LocationRequest LocRequest;
+    private GoogleApiClient gApiClient;
+    private Marker currLocMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +60,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // designing the navigation drawer layout
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,25 +73,40 @@ public class MainActivity extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         assert drawer != null;
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         assert navigationView != null;
         navigationView.setNavigationItemSelectedListener(this);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) this.getSupportFragmentManager()
+
+        // Map code
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+        mapFragment = (SupportMapFragment) this.getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
 
         String appVersion = "v1";
-        Backendless.initApp( this, "5F7FFD88-9B9E-DB7C-FF62-E4212D969D00", "70C86C90-41F6-97BB-FF1C-99C48DC84E00", appVersion );
+        Backendless.initApp(this, "5F7FFD88-9B9E-DB7C-FF62-E4212D969D00", "70C86C90-41F6-97BB-FF1C-99C48DC84E00", appVersion);
 
 
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //stop location updates when Activity is no longer active
+        if (gApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(gApiClient, this);
+        }
     }
 
     @Override
@@ -110,7 +150,7 @@ public class MainActivity extends AppCompatActivity
         Intent itemIntent;
 
         if (id == R.id.nav_login) {
-            itemIntent= new Intent(MainActivity.this,LoginActivity.class);
+            itemIntent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(itemIntent);
         } else if (id == R.id.nav_manage) {
 
@@ -130,10 +170,144 @@ public class MainActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng maputo = new LatLng(-25.9699086,32.5739359);
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                gMap.setMyLocationEnabled(true);
+            }
+        }
+        else {
+            buildGoogleApiClient();
+            gMap.setMyLocationEnabled(true);
+        }
+
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        gApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        gApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLoc = location;
+        if (currLocMarker != null) {
+            currLocMarker.remove();
+        }
+
+        //Place current location marker
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("You are Here!");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+        currLocMarker = gMap.addMarker(markerOptions);
+
+        //move map camera
         float zoomIn= (float) 16.0;
-        gMap.addMarker(new MarkerOptions().position(maputo).title("Você está aqui"));
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(maputo,zoomIn));
+       gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoomIn));
+        gMap.animateCamera(CameraUpdateFactory.zoomTo(16));
+
+        //stop location updates
+        if (gApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(gApiClient, this);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocRequest = new LocationRequest();
+        LocRequest.setInterval(1000);
+        LocRequest.setFastestInterval(1000);
+        LocRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(gApiClient, LocRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    public boolean checkLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        if (gApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        gMap.setMyLocationEnabled(true);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
