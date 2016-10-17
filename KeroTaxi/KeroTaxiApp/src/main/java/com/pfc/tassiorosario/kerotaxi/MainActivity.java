606cd0bde2ad996916a80b0cbfda3c902ba36ef6
TRosario619 <tassiorosario@gmail.com>
2016-10-17 11:59:04 +0200

    able to search by distance and send notification to related devices

--- KeroTaxi/KeroTaxiApp/src/main/java/com/pfc/tassiorosario/kerotaxi/MainActivity.java
+++ KeroTaxi/KeroTaxiApp/src/main/java/com/pfc/tassiorosario/kerotaxi/MainActivity.java
@@ -2,10 +2,12 @@ package com.pfc.tassiorosario.kerotaxi;
 
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -28,8 +30,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.backendless.Backendless;
+import com.backendless.BackendlessCollection;
 import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
+import com.backendless.messaging.DeliveryOptions;
+import com.backendless.messaging.PublishOptions;
+import com.backendless.persistence.BackendlessDataQuery;
+import com.backendless.persistence.QueryOptions;
+import com.backendless.services.messaging.MessageStatus;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -45,6 +56,9 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.login2.DefaultCallback;
 import com.login2.Defaults;
+import com.pfc.tassiorosario.kerotaxi.model.Taxista;
+
+import java.util.Iterator;
 
 
 public class MainActivity extends AppCompatActivity
@@ -64,12 +78,18 @@ public class MainActivity extends AppCompatActivity
     private LatLng latLng;
     private TextView nomeUsuario;
     private TextView usuario;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Backendless.setUrl( Defaults.SERVER_URL );
-        Backendless.initApp( this, Defaults.APPLICATION_ID, Defaults.SECRET_KEY, Defaults.VERSION );
+        Backendless.setUrl(Defaults.SERVER_URL);
+        Backendless.initApp(this, Defaults.APPLICATION_ID, Defaults.SECRET_KEY, Defaults.VERSION);
         bu = Backendless.UserService.CurrentUser();
 
 
@@ -83,14 +103,17 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(bu==null){
+                if (bu == null) {
                     Snackbar.make(view, "Faça o Login para habilitar esta função", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show(); 
-                }else{
-                    Snackbar.make(view, "Olá", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
+                } else {
+
+
+                    callCab();
+
+                    //System.out.println(lastLoc.getLatitude()+", "+lastLoc.getLongitude()+": yes");
                 }
-                
+
             }
         });
 
@@ -105,30 +128,27 @@ public class MainActivity extends AppCompatActivity
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         assert navigationView != null;
         navigationView.setNavigationItemSelectedListener(this);
-        Menu nav_menu =navigationView.getMenu();
+        Menu nav_menu = navigationView.getMenu();
         View headerView = navigationView.inflateHeaderView(R.layout.nav_header_main);
 
-        nomeUsuario= (TextView) headerView.findViewById(R.id.welcomeTxtV);
-        usuario= (TextView) headerView.findViewById(R.id.userTxtV);
-        if(bu==null){
+        nomeUsuario = (TextView) headerView.findViewById(R.id.welcomeTxtV);
+        usuario = (TextView) headerView.findViewById(R.id.userTxtV);
+        if (bu == null) {
 
             nav_menu.findItem(R.id.nav_edit_user).setVisible(false);
             nav_menu.findItem(R.id.nav_history).setVisible(false);
             nav_menu.findItem(R.id.nav_logout).setVisible(false);
-        }else{
+        } else {
             nav_menu.findItem(R.id.nav_login).setVisible(false);
-            nomeUsuario.setText((String)bu.getProperty("name"));
+            nomeUsuario.setText((String) bu.getProperty("name"));
             usuario.setText(bu.getEmail());
         }
 
         invalidateOptionsMenu();
 
 
-
-
-
         // Map code
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             checkLocationPermission();
         }
         mapFragment = (SupportMapFragment) this.getSupportFragmentManager()
@@ -136,7 +156,72 @@ public class MainActivity extends AppCompatActivity
         mapFragment.getMapAsync(this);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    private void callCab() {
+
+        String query = "distance(%s, %s, localizacaoActual.latitude,localizacaoActual.longitude)< km(5)";
+        String whereClause = String.format(query,lastLoc.getLatitude(),lastLoc.getLongitude());
+
+        BackendlessDataQuery dataQuery = new BackendlessDataQuery(whereClause);
+        QueryOptions queryOptions= new QueryOptions();
+        queryOptions.addRelated("localizacaoActual");
+        dataQuery.setQueryOptions(queryOptions);
+
+        progressDialog = ProgressDialog.show(MainActivity.this, "","A localizar taxistas...", true);
+        Backendless.Data.of(Taxista.class).find(dataQuery, new AsyncCallback<BackendlessCollection<Taxista>>() {
+            @Override
+            public void handleResponse(BackendlessCollection<Taxista> taxistas) {
+                progressDialog.cancel();
+                if(taxistas.getCurrentPage().size()==0){
+                    progressDialog.cancel();
+                    System.out.println("no taxi drivers");
+                }
+
+                Toast.makeText(MainActivity.this, "Success: "+taxistas.getTotalObjects(), Toast.LENGTH_SHORT).show();
+                progressDialog = ProgressDialog.show(MainActivity.this, "","A enviar pedido...", true);
+                Iterator<Taxista> iterator = taxistas.getCurrentPage().iterator();
+                DeliveryOptions deliveryOptions = new DeliveryOptions();
+
+                while( iterator.hasNext() )
+                {
+                    Taxista taxista = iterator.next();
+                    System.out.println(String.format("Taxista: %s",taxista.getIdUsuario()) );
+                    deliveryOptions.addPushSinglecast(taxista.getRegDispositivo());
+                }
+
+                PublishOptions publishOptions = new PublishOptions();
+                publishOptions.putHeader( PublishOptions.ANDROID_TICKER_TEXT_TAG, getString( R.string.app_name ) );
+                publishOptions.putHeader( PublishOptions.ANDROID_CONTENT_TITLE_TAG,"Um novo passageiro para si");
+                publishOptions.putHeader( PublishOptions.ANDROID_CONTENT_TEXT_TAG, "Olá");
+
+
+                Backendless.Messaging.publish("this is a private message", publishOptions, deliveryOptions, new AsyncCallback<MessageStatus>() {
+                    @Override
+                    public void handleResponse(MessageStatus messageStatus) {
+                        progressDialog.cancel();
+                        Toast.makeText(MainActivity.this, "Pendido enviado", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault backendlessFault) {
+                        progressDialog.cancel();
+                        System.out.println("Server error: "+backendlessFault.getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void handleFault(BackendlessFault backendlessFault) {
+                   progressDialog.cancel();
+                   System.out.println("Server error: "+backendlessFault.getMessage());
+            }
+        });
+    }
+
 
     @Override
     protected void onPause() {
@@ -151,7 +236,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onRestart() {
         super.onRestart();
-        Intent previous= new Intent(this, MainActivity.class);
+        Intent previous = new Intent(this, MainActivity.class);
         startActivity(previous);
         this.finish();
     }
@@ -161,7 +246,7 @@ public class MainActivity extends AppCompatActivity
 
         if (requestCode == 1) {
 
-            if(resultCode == RESULT_OK){
+            if (resultCode == RESULT_OK) {
                 this.finish();
             }
             if (resultCode == RESULT_CANCELED) {
@@ -215,9 +300,9 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_login) {
             itemIntent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(itemIntent);
-        } else if (id ==R.id.nav_edit_user) {
+        } else if (id == R.id.nav_edit_user) {
 
-        }else if (id == R.id.nav_history) {
+        } else if (id == R.id.nav_history) {
 
         } else if (id == R.id.nav_manage) {
 
@@ -225,8 +310,8 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_about) {
 
-        }else if (id == R.id.nav_logout) {
-            new AlertDialog.Builder(MainActivity.this).setMessage("Deseja realmente sair?").setNegativeButton("Não",null).setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+        } else if (id == R.id.nav_logout) {
+            new AlertDialog.Builder(MainActivity.this).setMessage("Deseja realmente sair?").setNegativeButton("Não", null).setPositiveButton("Sim", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     onLogoutItemClicked();
@@ -241,27 +326,23 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public void onLogoutItemClicked()
-    {
-        Backendless.UserService.logout(new DefaultCallback<Void>( MainActivity.this,"A sair..." )
-        {
+    public void onLogoutItemClicked() {
+        Backendless.UserService.logout(new DefaultCallback<Void>(MainActivity.this, "A sair...") {
             @Override
-            public void handleResponse( Void response )
-            {
-                super.handleResponse( response );
-                startActivity( new Intent( MainActivity.this, MainActivity.class ) );
+            public void handleResponse(Void response) {
+                super.handleResponse(response);
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
                 finish();
             }
 
             @Override
-            public void handleFault( BackendlessFault fault )
-            {
-                if( fault.getCode().equals( "3023" ) ) // Unable to logout: not logged in (session expired, etc.)
-                    handleResponse( null );
+            public void handleFault(BackendlessFault fault) {
+                if (fault.getCode().equals("3023")) // Unable to logout: not logged in (session expired, etc.)
+                    handleResponse(null);
                 else
-                    super.handleFault( fault );
+                    super.handleFault(fault);
             }
-        } );
+        });
 
     }
 
@@ -270,15 +351,14 @@ public class MainActivity extends AppCompatActivity
         gMap = googleMap;
 
         //Initialize Google Play Services
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED) {
                 buildGoogleApiClient();
                 gMap.setMyLocationEnabled(true);
             }
-        }
-        else {
+        } else {
             buildGoogleApiClient();
             gMap.setMyLocationEnabled(true);
         }
@@ -311,8 +391,8 @@ public class MainActivity extends AppCompatActivity
         currLocMarker = gMap.addMarker(markerOptions);
 
         //move map camera
-        float zoomIn= (float) 16.0;
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoomIn));
+        float zoomIn = (float) 16.0;
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomIn));
         gMap.animateCamera(CameraUpdateFactory.zoomTo(16));
 
         //stop location updates
@@ -344,7 +424,7 @@ public class MainActivity extends AppCompatActivity
         Toast.makeText(MainActivity.this, "Problem", Toast.LENGTH_SHORT).show();
     }
 
-    public boolean checkLocationPermission(){
+    public boolean checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -377,7 +457,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+                                           @NonNull String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
@@ -402,11 +482,51 @@ public class MainActivity extends AppCompatActivity
                     // functionality that depends on this permission.
                     Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
                 }
-                return;
             }
 
             // other 'case' lines to check for other
             // permissions this app might request
         }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.pfc.tassiorosario.kerotaxi/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.pfc.tassiorosario.kerotaxi/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
